87675d98064d81fc34722012489d7083832f7890
ChristineWest <christinewwwest@gmail.com>
2022-09-18 16:27:13 -0400

    added logic for checking winning combinations

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const cellElements = document.querySelectorAll("[data-cell]");
 const boardElement = document.getElementById("board");
@@ -18,6 +28,9 @@ function handleClick(e) {
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   //place Mark
   placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  }
   //check for win
   //check for draw
   //switch turns
@@ -39,4 +52,12 @@ function setBoardHoverClass() {
   } else {
     boardElement.classList.add(X_CLASS);
   }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
